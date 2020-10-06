df587b96f5b196a0ce487830c043919350bf66ef
Ismael Gonzalez Risco <40613999+Dev0nair@users.noreply.github.com>
2020-10-06 10:01:12 +0200

    Se ha añadido las constantes con las rutas donde se almacenara las cosas en firestore

---
+++ app/src/main/java/com/ismaelgr/winkle/util/Routes.kt
@@ -0,0 +1,11 @@
+package com.ismaelgr.winkle.util
+
+object Routes {
+    const val INCIDENCIAS = "incidencias"
+    const val PRODUCTOS = "productos"
+    const val FAVORITOS = "favoritos"
+    const val CESTAS = "cestas"
+    const val COMPRAS = "compras"
+    const val PERFILES = "perfiles"
+    const val REPORTS = "reports"
+}
