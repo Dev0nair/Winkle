20e05b6624bf4b20c1ad96d130378a7bbf0d6473
Ismael Gonzalez Risco <40613999+Dev0nair@users.noreply.github.com>
2020-10-06 09:49:56 +0200

    Se ha conectado con Firebase

--- app/src/main/java/com/ismaelgr/winkle/MainActivity.kt
+++ app/src/main/java/com/ismaelgr/winkle/MainActivity.kt
@@ -2,11 +2,22 @@ package com.ismaelgr.winkle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeFirebase()
+    }
+
+
+    private fun initializeFirebase() {
+        FirebaseApp.initializeApp(this).run {
+            Log.i("MAIN", "Se ha inicializado firebase!")
+        }
     }
 }
