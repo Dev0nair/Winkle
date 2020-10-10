8f05134fca34ca98c738420413a9e94d97c33aac
Ismael Gonzalez Risco <40613999+Dev0nair@users.noreply.github.com>
2020-10-11 00:52:01 +0200

    Se ha añadido la logica de la pantalla de legal

--- app/src/main/java/com/ismaelgr/winkle/presentation/legalscreen/LegalScreenFragment.kt
+++ app/src/main/java/com/ismaelgr/winkle/presentation/legalscreen/LegalScreenFragment.kt
@@ -1,8 +1,11 @@
 package com.ismaelgr.winkle.presentation.legalscreen
 
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ismaelgr.winkle.presentation.base.BaseFragment
 import com.ismaelgr.winkle.R
+import com.ismaelgr.winkle.domain.usecase.LegalConfirmationUseCase
+import kotlinx.android.synthetic.main.fragment_legalscreen.*
 
 /**
  * A simple [Fragment] subclass.
@@ -11,7 +14,14 @@ class LegalScreenFragment : BaseFragment(R.layout.fragment_legalscreen), LegalSc
 
     private lateinit var legalscreenPresenter: LegalScreenContract.Presenter
 
+    override fun loadLogin() {
+        findNavController().navigate(R.id.action_legalScreenFragment_to_loginFragment)
+    }
+
     override fun initElements() {
-        legalscreenPresenter = LegalScreenPresenter(this as LegalScreenContract.View)
+        legalscreenPresenter =
+            LegalScreenPresenter(this as LegalScreenContract.View, LegalConfirmationUseCase())
+
+        legal_accept_btn.setOnClickListener { legalscreenPresenter.onAcceptButtonClick() }
     }
 }
--- app/src/main/java/com/ismaelgr/winkle/presentation/legalscreen/LegalScreenPresenter.kt
+++ app/src/main/java/com/ismaelgr/winkle/presentation/legalscreen/LegalScreenPresenter.kt
@@ -1,8 +1,17 @@
 package com.ismaelgr.winkle.presentation.legalscreen
 
+import com.ismaelgr.winkle.domain.usecase.LegalConfirmationUseCase
 import com.ismaelgr.winkle.presentation.base.BasePresenter
 
-class LegalScreenPresenter(view: LegalScreenContract.View) :
-    BasePresenter<LegalScreenContract.View>(view), LegalScreenContract.Presenter {
+class LegalScreenPresenter(
+    private val legal: LegalScreenContract.View,
+    private val legalConfirmationUseCase: LegalConfirmationUseCase
+) :
+    BasePresenter<LegalScreenContract.View>(legal), LegalScreenContract.Presenter {
 
+    override fun onAcceptButtonClick() {
+        legalConfirmationUseCase.apply {
+            legal.loadLogin()
+        }
+    }
 }
