6119ff18ccb796f34fd8aa21686c6f9635a0fabf
Ismael Gonzalez Risco <40613999+Dev0nair@users.noreply.github.com>
2020-10-11 00:50:50 +0200

    Se metio la constante para la clave de la sharedpreference

--- app/src/main/java/com/ismaelgr/winkle/util/Consts.kt
+++ app/src/main/java/com/ismaelgr/winkle/util/Consts.kt
@@ -1,6 +1,7 @@
 package com.ismaelgr.winkle.util
 
 object Consts {
+    internal const val SHARED_LEGAL_KEY = "legalConfirmation"
 
     internal const val SPLASH_DURATION = 1200L // 1,2 segundos de duracion
 }
