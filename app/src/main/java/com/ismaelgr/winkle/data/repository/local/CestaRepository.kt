dccf3f5da504adc06ad560a85930c2566b91d76a
Ismael Gonzalez Risco <40613999+Dev0nair@users.noreply.github.com>
2020-10-25 09:58:55 +0100

    Se ha cambiado el maybe<any> por el completable

--- app/src/main/java/com/ismaelgr/winkle/data/repository/local/CestaRepository.kt
+++ app/src/main/java/com/ismaelgr/winkle/data/repository/local/CestaRepository.kt
@@ -2,6 +2,7 @@ package com.ismaelgr.winkle.data.repository.local
 
 import com.ismaelgr.winkle.data.entity.Cesta
 import com.ismaelgr.winkle.data.repository.needs.CestaRepositoryNeed
+import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Maybe
 
 class CestaRepository: CestaRepositoryNeed {
@@ -14,23 +15,23 @@ class CestaRepository: CestaRepositoryNeed {
     override fun getCesta(idProfile: String): Maybe<Cesta> =
         Maybe.just(cesta)
 
-    override fun addToCesta(idProfile: String, idProduct: String): Maybe<Any> {
+    override fun addToCesta(idProfile: String, idProduct: String): Completable {
         cesta.products.add(idProduct)
-        return Maybe.just(Any())
+        return Completable.complete()
     }
 
-    override fun clearCesta(idProfile: String): Maybe<Any> {
+    override fun clearCesta(idProfile: String): Completable {
         cesta.products.clear()
-        return Maybe.just(Any())
+        return Completable.complete()
     }
 
     override fun deleteFromCesta(
         idProfile: String,
         idProduct: String
-    ): Maybe<Any> = Maybe.create { emitter ->
+    ): Completable = Completable.create { emitter ->
         if(cesta.products.contains(idProduct)) {
             cesta.products.remove(idProduct)
-            emitter.onSuccess(Any())
+            emitter.onComplete()
         } else {
             emitter.onError(Error("No se pudo borrar un producto que no existe"))
         }
--- app/src/main/java/com/ismaelgr/winkle/data/repository/needs/CestaRepositoryNeed.kt
+++ app/src/main/java/com/ismaelgr/winkle/data/repository/needs/CestaRepositoryNeed.kt
@@ -1,15 +1,16 @@
 package com.ismaelgr.winkle.data.repository.needs
 
 import com.ismaelgr.winkle.data.entity.Cesta
+import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Maybe
 
 interface CestaRepositoryNeed {
 
     fun getCesta(idProfile: String): Maybe<Cesta>
 
-    fun addToCesta(idProfile: String, idProduct: String): Maybe<Any>
+    fun addToCesta(idProfile: String, idProduct: String): Completable
 
-    fun clearCesta(idProfile: String): Maybe<Any>
+    fun clearCesta(idProfile: String): Completable
 
-    fun deleteFromCesta(idProfile: String, idProduct: String): Maybe<Any>
+    fun deleteFromCesta(idProfile: String, idProduct: String): Completable
 }
